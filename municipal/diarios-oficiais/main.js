d87bd28043546902d7352f0c5da9d7aa76f9ca47
tributosmag <tributos@mag.com.br>
2023-06-20 12:23:48 -0300

    .

--- municipal/diarios-oficiais/main.js
+++ municipal/diarios-oficiais/main.js
@@ -1,24 +1,22 @@
 const description = document.querySelector(".tooltip");
+const paths = document.querySelectorAll('path');
+let tabelaVisivel = false;
 
-		document.querySelectorAll('path').forEach((el) =>
-			el.addEventListener('mouseover', (event) => {
-				event.target.className = ("enabled");
-				description.classList.add("active");
-				description.innerHTML = event.target.id;
-			})
+paths.forEach((el) =>
+  el.addEventListener('mouseover', (event) => {
+    if (!tabelaVisivel) {
+      event.target.classList.add("enabled");
+      description.classList.add("active");
+      description.innerHTML = event.target.id;
+    }
+  })
+);
 
-		);
-
-		document.querySelectorAll('path').forEach((el) =>
-			el.addEventListener("mouseout", () => {
-				description.classList.remove("active");
-			})
-		);
-
-		document.onmousemove = function (e) {
-			description.style.left = e.pageX + "px";
-			description.style.top = (e.pageY - 70) + "px";
-		}
+paths.forEach((el) =>
+  el.addEventListener("mouseout", () => {
+    description.classList.remove("active");
+  })
+);
 
 $(document).ready(function() {
   $("a.botao-estado").click(function(event) {
@@ -26,6 +24,8 @@ $(document).ready(function() {
     var tabela = $(this).data("tabela");
     $(".tabela-estados").hide();
     $("#" + tabela).show();
+    tabelaVisivel = true;
+    description.classList.remove("active");
   });
 
   $("path").click(function(event) {
@@ -33,71 +33,45 @@ $(document).ready(function() {
     var tabela = $(this).data("tabela");
     $(".tabela-estados").hide();
     $("#" + tabela).show();
+    tabelaVisivel = true;
+    description.classList.remove("active");
   });
 
-
-  // Registra cliques
   $(document).on("click", ".tabela-estados tr", function() {
     $(this).toggleClass("selecionado");
   });
-});
 
-$(document).ready(function() {
-  $(".botao-estado").click(function(event) {
-    event.preventDefault();
-    var tabelaId = $(this).attr("data-tabela");
-
-    // Oculta o mapa e as tabelas
-    $("svg").hide();
-    $(".tabela-estados").hide();
-
-    // Move a tabela correspondente para o novo local
-    $("#" + tabelaId).appendTo("#container").show();
-  });
-
-  $("path").click(function(event) {
-    event.preventDefault();
-    var tabelaId = $(this).attr("data-tabela");
-
-    // Oculta o mapa e as tabelas
-    $("svg").hide();
-    $(".tabela-estados").hide();
-
-    // Move a tabela correspondente para o novo local
-    $("#" + tabelaId).appendTo("#container").show();
-  });
-});
-
-$(document).ready(function() {
   $("#botao-mapa").click(function(event) {
     event.preventDefault();
-
-    // Oculta as tabelas e mostra o mapa
     $(".tabela-estados").hide();
     $("#mapa").show();
+    tabelaVisivel = false;
   });
 
   $(".botao-estado").click(function(event) {
     event.preventDefault();
     var tabelaId = $(this).attr("data-tabela");
-
-    // Oculta o mapa e as tabelas
-    $("#mapa").hide();
+    $("svg").hide();
     $(".tabela-estados").hide();
-
-    // Move a tabela correspondente para o novo local
     $("#" + tabelaId).appendTo("#container").show();
+    tabelaVisivel = true;
+    description.classList.remove("active");
   });
 
   $("path").click(function(event) {
     event.preventDefault();
     var tabelaId = $(this).attr("data-tabela");
-
-    // Oculta o mapa e as tabelas
-    $("#mapa").hide();
+    $("svg").hide();
     $(".tabela-estados").hide();
-
-    // Move a tabela correspondente para o novo local
     $("#" + tabelaId).appendTo("#container").show();
+    tabelaVisivel = true;
+    description.classList.remove("active");
   });
 });
+
+document.onmousemove = function(e) {
+  if (!tabelaVisivel) {
+    description.style.left = e.pageX + "px";
+    description.style.top = (e.pageY - 70) + "px";
+  }
+};
